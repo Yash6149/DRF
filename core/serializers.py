9d35eff10e08da34344f3fc55a99b521d8dc715d
Yash6149 <yashp6149@gmail.com>
2018-01-17 15:25:43 +0530

    serializer

---
+++ core/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import University, Student
+
+class UniversitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = University
+        fields = ('id','name')
+
+class StudentSerializer(serializers.ModelSerializer):
+	#university = UniversitySerializer(read_only=True)
+	class Meta:
+		model = Student
+		fields = ('id','first_name','last_name','university')
