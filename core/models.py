40cd6bd9ead3464da0e1dec25f953ca4f6b6e660
Yash6149 <yashp6149@gmail.com>
2018-01-17 15:24:30 +0530

    models

---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class University(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "University"
+        verbose_name_plural = "Universities"
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    university = models.ForeignKey(University,related_name='uid',on_delete="")
+
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+
+    def __unicode__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+   
+    def __str__(self):
+        return self.name
